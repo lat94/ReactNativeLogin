e8a7317fb75e951cadc3d43f19342ba90c36aed5
lat94 <lucas.tx94@hotmail.com>
2018-05-22 00:16:56 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+import Login from './components/Login';
+
+AppRegistry.registerComponent('ReactNativeProjectLogin', () => Login);
